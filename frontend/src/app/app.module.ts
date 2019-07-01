a9cd3fd5c6970e99e44b303bd6d61652faf7f3c5
Cj <srikrishna.cj@gmail.com>
2019-07-02 00:35:32 +0530

    progress

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+
+import {MyOwnCustomMaterialModule} from './CustomMaterialModule'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MyOwnCustomMaterialModule,
     AppRoutingModule
   ],
   providers: [],
